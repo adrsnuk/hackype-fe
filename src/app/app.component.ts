fa93d759476f518afedc6ef7d1793c139888792f
snuk <snuk.knus@gmail.com>
2023-03-02 07:56:18 +0200

    basic typing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'hackype';
+  
+  pressedKey = '';
+  textToType = 'Sign in to your account';
+  charsToType = this.textToType.split('');
+  currentPosition = 0;
+
+  @HostListener('document:keypress', ['$event'])
+  handleKeyboardEvent(event: KeyboardEvent) { 
+    this.pressedKey = event.key;
+  
+    this.currentPosition = (event.key == this.charsToType.at(this.currentPosition)) ? this.currentPosition+ 1 : 0;
+
+    console.warn(this.pressedKey);
+  }
 }
