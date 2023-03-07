0dd5b148292f1fe482e300a4b8380522c94b9deb
snuk <snuk.knus@gmail.com>
2023-03-07 07:57:46 +0200

    print text to type in console

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component, HostListener } from '@angular/core';
+import { Component, HostListener, Injectable } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -29,4 +31,18 @@ export class AppComponent {
     const typed = basic + "text-yellow-400 bg-green-300";
     return this.currentPosition > i ? typed : untyped;
   }
+
+  constructor(private http: HttpClient) { 
+    this.http.get('assets/intro.txt', { responseType: 'text' })
+          .subscribe(data => {
+            this.textToType = data; 
+            console.warn(this.textToType);
+          });
+  }
+
+
+//   ngOnInit(){
+//   this.httpClient.get('assets/Maze1.txt', { responseType: 'text' })
+//       .subscribe(data => mazeData = data);
+// }
 }
